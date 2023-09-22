b7aefa02774bf4d82dcac39270b4fc2c2bab9c6a
Fellipe <fellipe.souza@unifal-mg.edu.br>
2023-09-22 15:50:25 -0300

    feat: Added feature to add a new chore on ChoreService

--- todo/src/main/java/br/edu/unifalmg/service/ChoreService.java
+++ todo/src/main/java/br/edu/unifalmg/service/ChoreService.java
@@ -1,13 +1,39 @@
 package br.edu.unifalmg.service;
 
 import br.edu.unifalmg.domain.Chore;
+import br.edu.unifalmg.exception.DuplicatedChoreException;
+import br.edu.unifalmg.exception.InvalidDeadlineException;
+import br.edu.unifalmg.exception.InvalidDescriptionException;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 public class ChoreService {
 
+    private List<Chore> chores;
+
+    public ChoreService() {
+        chores = new ArrayList<>();
+    }
+
     public Chore addChore(String description, LocalDate deadline) {
-        return null;
+        if (Objects.isNull(description) || description.isEmpty()) {
+            throw new InvalidDescriptionException("The description cannot be null or empty");
+        }
+        if (Objects.isNull(deadline) || deadline.isBefore(LocalDate.now())) {
+            throw new InvalidDeadlineException("The deadline cannot be null or before the current date");
+        }
+        for (Chore chore : chores) {
+            if (chore.getDescription().equals(description)
+                    && chore.getDeadline().isEqual(deadline)) {
+                throw new DuplicatedChoreException("The given chore already exists.");
+            }
+        }
+        Chore chore = new Chore(description, Boolean.FALSE, deadline);
+        chores.add(chore);
+        return chore;
     }
 
 }
